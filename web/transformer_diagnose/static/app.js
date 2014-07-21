31044ab0c4304c52fa732275105a0a45d23b90ce
caster blue <caster@ubuntu.(none)>
2014-07-21 10:35:04 +0800

    1.test code of ExtJS

--- web/transformer_diagnose/static/app.js
+++ web/transformer_diagnose/static/app.js
@@ -1,8 +1,16 @@
-Ext.create('Ext.Panel', {
-     renderTo     : Ext.getBody(),
-     width        : 200,
-     height       : 150,
-     bodyPadding  : 5,
-     title        : 'Hello World',
-     html         : 'Hello <b>World</b>...'
- });
+Ext.application({
+    name   : 'MyApp',
+
+    launch : function() {
+
+       Ext.create('Ext.Panel', {
+            renderTo     : Ext.getBody(),
+            width        : 200,
+            height       : 150,
+            bodyPadding  : 5,
+            title        : 'Hello World',
+            html         : 'Hello <b>World</b>...'
+        });
+
+    }
+});
